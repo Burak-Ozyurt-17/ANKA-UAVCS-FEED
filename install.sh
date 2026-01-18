5fc3875779e37e5fecfdbd5df813bf702f626dd8
Burak-Ozyurt-17 <burakozyurt2010@gmail.com>
2026-01-18 14:38:28 +0300

    Changed installing procedure with a shell script

---
+++ install.sh
@@ -0,0 +1,4 @@
+python -m pip install --upgrade pip --break-system-packages
+pip install ultralytics --no-deps --break-system-packages
+pip install numpy opencv-python-headless pillow flask flask-cors inputs --break-system-packages
+pip install torch torchvision --index-url https://download.pytorch.org/whl/cpu --break-system-packages
